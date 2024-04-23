b688e3596058a2cb2fa7aae2d7d23e6e6ecec57c
Ming <limingyuan6@qq.com>
2024-04-23 23:14:43 +0800

    Update streamlit_app.py

--- DataWhale/llm_universe/streamlit_test/streamlit_app.py
+++ DataWhale/llm_universe/streamlit_test/streamlit_app.py
@@ -5,19 +5,19 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 import sys
-sys.path.append("../C3 搭建知识库") # 将父目录放入系统路径中
+# sys.path.append("../C3 搭建知识库") # 将父目录放入系统路径中
 from zhipuai_embedding import ZhipuAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())    # read local .env file
+# _ = load_dotenv(find_dotenv())    # read local .env file
 from zhipuai_llm import ZhipuAILLM
 
 
 #export OPENAI_API_KEY=
 #os.environ["OPENAI_API_BASE"] = 'https://api.chatgptid.net/v1'
-zhipuai_api_key = os.environ['ZHIPUAI_API_KEY']
+# zhipuai_api_key = os.environ['ZHIPUAI_API_KEY']
 
 
 # def generate_response(input_text, openai_api_key):
